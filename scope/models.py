53ce7b5fd68bec946b2d3f5a7f9ca75a5f8a7e27
Ahmed Maher <ahmedmaherelmitwally@gmail.com>
2017-04-26 17:29:07 +0200

    add social links to speaker model

--- scope/models.py
+++ scope/models.py
@@ -6,7 +6,8 @@ class Speaker(models.Model):
     title = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     image = models.CharField(max_length=200, null=True, editable=False)
-
+    social = models.CharField(max_length=500, null=True, blank=True)
+    
     def __str__(self):
         return "Speaker {}: {}".format(self.id, self.name)
 
